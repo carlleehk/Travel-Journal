6932144b03c454b1e6dd34af801f53bb7de2dd8d
Carl Lee <carllee.hk@gmail.com>
2017-03-26 18:54:06 -0400

    begins choosing picture UI

---
+++ Travel Journal/PictureViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PictureViewController.swift
+//  Travel Journal
+//
+//  Created by Carl Lee on 3/25/17.
+//  Copyright © 2017 Carl Lee. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func takePicture(_ sender: Any) {
+        
+    }
+
+    @IBAction func Album(_ sender: Any) {
+    }
+    
+    @IBAction func FlickR(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
