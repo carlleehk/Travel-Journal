725d1c25061752dadeebc130c6decd5f8309d8f2
Carl Lee <carllee.hk@gmail.com>
2017-04-01 21:32:09 -0400

    Add action when select a cell on table view

--- Travel Journal/JournalTableViewController.swift
+++ Travel Journal/JournalTableViewController.swift
@@ -7,11 +7,30 @@
 //
 
 import UIKit
+import CoreData
 
-class JournalTableViewController: UIViewController {
+class JournalTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let stack = (UIApplication.shared.delegate as! AppDelegate).stack
+    let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Name")
+    var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>?{
+        didSet{
+            fetchedResultsController?.delegate = self
+            executeSearch()
+        }
+    
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        fr.sortDescriptors = [NSSortDescriptor(key: "journalName", ascending: true)]
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fr, managedObjectContext: stack.context, sectionNameKeyPath: nil, cacheName: nil)
 
         // Do any additional setup after loading the view.
     }
@@ -25,6 +44,63 @@ class JournalTableViewController: UIViewController {
         presentAlert()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let fc = fetchedResultsController{
+            print("there are \(fc.sections![section].numberOfObjects) objects")
+            return fc.sections![section].numberOfObjects
+        } else{
+            return 0
+
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //FIND THE NOTEBOOK
+        let nb = fetchedResultsController?.object(at: indexPath) as! Name
+        
+        //CREATE THE CELL
+        let cell = tableView.dequeueReusableCell(withIdentifier: "JournalCell", for: indexPath)
+        
+        //SYNC NOTEBOOK -> CELL
+        cell.textLabel?.text = nb.journalName
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        let alertController = UIAlertController(title: nil, message: "Select the action you wanted to perform for \((cell?.textLabel?.text)!)", preferredStyle: .alert)
+        let editAction = UIAlertAction(title: "Edit", style: .default){(_) in
+            
+            let control = self.storyboard?.instantiateViewController(withIdentifier: "detailJournal") as! DetailJournalViewController
+            self.present(control, animated: true, completion: nil)
+            
+        }
+        let deleteAction = UIAlertAction(title: "Delete", style: .default){(_) in
+            
+            self.stack.context.delete(self.fetchedResultsController?.object(at: indexPath) as! Name)
+            do{
+                try self.stack.saveContext()
+            }catch{
+                print("Error while saving")
+            }
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(editAction)
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+
+     
+    }
+    
     func presentAlert(){
         
         let alertController = UIAlertController(title: "New Journal", message: "Enter your journal name", preferredStyle: .alert)
@@ -32,7 +108,18 @@ class JournalTableViewController: UIViewController {
         let confirmAction = UIAlertAction(title: "Confirm", style: .default) { (_) in
             
             if let field = alertController.textFields?[0]{
-                print(field.text!)
+                if field.text != ""{
+                    let nl = Name(name: field.text!, context: self.stack.context)
+                    do{
+                        try self.stack.saveContext()
+                    }catch{
+                        print("Error while saving")
+                    }
+                    print(nl)
+                }else{
+                    self.presentAlert()
+                }
+                
             } else{
                 print("error")
             }
@@ -50,6 +137,19 @@ class JournalTableViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
         
     }
+    
+    func executeSearch(){
+        
+        if let fc = fetchedResultsController{
+            
+            do{
+                try fc.performFetch()
+            } catch let e as NSError{
+                print("Error while trying to perform a search: \n\(e)\n\(fetchedResultsController)")
+            }
+        }
+    }
+
 
     /*
     // MARK: - Navigation
@@ -62,3 +162,45 @@ class JournalTableViewController: UIViewController {
     */
 
 }
+
+extension JournalTableViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        
+        let set = IndexSet(integer: sectionIndex)
+        
+        switch (type) {
+        case .insert:
+            tableView.insertSections(set, with: .fade)
+        case .delete:
+            tableView.deleteSections(set, with: .fade)
+        default:
+            // irrelevant in our case
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch(type) {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .update:
+            tableView.reloadRows(at: [indexPath!], with: .fade)
+        case .move:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+}
+
